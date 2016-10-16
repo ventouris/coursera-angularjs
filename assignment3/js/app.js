1fdf0778af49089745d6afe6947c8cc4c5b2cfa6
Tasos Ventouris <info@infogrideo.com>
2016-10-16 15:38:53 +0300

    assignment3

---
+++ assignment3/js/app.js
@@ -0,0 +1,64 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', foundItemsDirective)
+.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+
+
+
+function foundItemsDirective() {
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+      foundItems: '<',
+      onRemove: '&'
+    },
+    controller: NarrowItDownController,
+    controllerAs: 'narrow',
+    bindToController: true
+  };
+
+  return ddo;
+}
+
+
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+
+  var narrowIt = this;
+
+  narrowIt.searchMenu = function () {
+    var found = MenuSearchService.getMatchedMenuItems(narrowIt.searchTerm);
+  }
+
+}
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath']
+function MenuSearchService($http, ApiBasePath) {
+
+  var service = this;
+
+  service.getMatchedMenuItems = function (searchTeam) {
+    return $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json")
+    }).then(function (result) {
+      var foundItems = [];
+
+      for (var i = 0; i < result.data.menu_items.length; i++) {
+        var descr = result.data.menu_items[i].description;
+        if (descr.toLowerCase().indexOf(searchTeam) !== -1) {
+          foundItems.push(result.data.menu_items[i]);
+        }
+      }
+      return foundItems;
+    })
+
+  }
+}
+
+})();
